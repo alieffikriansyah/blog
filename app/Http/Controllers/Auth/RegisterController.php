953799c5410cbccbc36fab2628b90a02acd72e8e
Aryaak <aryarizkytriputra2303043@gmail.com>
2023-08-14 21:10:12 +0700

    fix login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -24,6 +24,11 @@ class RegisterController extends Controller
 
     use RegistersUsers;
 
+    public function showRegistrationForm()
+    {
+        return view('auth.regis');
+    }
+
     /**
      * Where to redirect users after registration.
      *
