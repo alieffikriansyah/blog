7b2d21dc49cb92d1b4dbdc2f626ddf0a86885efb
Farhan Ramadhan <farhanramadhansk@gmail.com>
2023-09-09 18:21:39 +0700

    notifikasi presensi

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -74,7 +74,7 @@
         </a>
         <div class="collapse" id="charts">
             <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="{{route('penjualan')}}">Penjulan Saat ini</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{route('penjualan')}}">Penjualan Saat ini</a></li>
             </ul>
         </div>
     </li>
@@ -98,5 +98,13 @@
     </li>
     @endif
 
+    <!-- NOTIFIKASI PENINGAT PRESENSI -- START -->
+    @if (date('H') >= 7 && date('H') < 10)
+        <li class="nav-item">
+            <div class="badge badge-danger">Harap mengisi presensi</div>
+            <div class="badge badge-danger">sebelum 10.00 !</div>
+        </li>
+    @endif
+    <!-- NOTIFIKASI PENINGAT PRESENSI -- END -->
     </ul>
 </nav>
